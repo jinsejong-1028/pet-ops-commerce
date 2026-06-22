1474345f50eadffef611d9f7f2e782a0dc48de83
linkthelab <akwnsldj1@linkthelab.co.kr>
2026-06-22 18:13:06 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/petopscommerce/PetOpsCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.petopscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetOpsCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetOpsCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petopscommerce/PetOpsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petopscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetOpsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
